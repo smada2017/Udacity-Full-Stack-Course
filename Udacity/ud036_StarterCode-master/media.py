c88ed7d68fcf4330e3d8e5ef4ba9714ac101e820
smada2017 <saivardhanmada@gmail.com>
2018-02-07 20:22:47 -0500

    fixed errors with media file

--- Udacity/ud036_StarterCode-master/media.py
+++ Udacity/ud036_StarterCode-master/media.py
@@ -2,6 +2,6 @@ class Movie():
     """python class that generalizes a typical movie with attributes"""
     def __init__(self, movie_title, poster_img, trailer):
         '''defines all the attributes of the movie class'''
-    self.title = movie_title
-    self.poster_image_url = poster_img
-    self.trailer_youtube_url = trailer
+    	self.title = movie_title
+    	self.poster_image_url = poster_img
+    	self.trailer_youtube_url = trailer
