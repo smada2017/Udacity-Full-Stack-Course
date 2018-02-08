b571ce1154105f3c94f1b42f71c16c13d73f09d2
smada2017 <saivardhanmada@gmail.com>
2018-02-08 10:20:04 -0500

    fixed trailer error

--- Udacity/ud036_StarterCode-master/entertainment_center.py
+++ Udacity/ud036_StarterCode-master/entertainment_center.py
@@ -8,11 +8,11 @@ fav_movies = []
 """creating and appending movie objects"""
 inception = media.Movie("Inception",
                         "https://tinyurl.com/za7f966",
-                        "https://www.youtube.com/watch?v=YoHD9XEInc0")
+                        "https://www.youtube.com/watch?v=8hP9D6kZseM")
 fav_movies.append(inception)
 godFather = media.Movie("The Godfather",
                         "https://tinyurl.com/ydenxa29",
-                        "https://www.youtube.com/watch?v=dNE2PvbesQU")
+                        "https://www.youtube.com/watch?v=kTlhEX0kmL8")
 fav_movies.append(godFather)
 avengers = media.Movie("Avengers",
                        "https://tinyurl.com/ycdnw5bn",
@@ -20,7 +20,7 @@ avengers = media.Movie("Avengers",
 fav_movies.append(avengers)
 starWarsForceAwaken = media.Movie("Star Wars: The Force Awakens",
                                   "https://tinyurl.com/y779v6wr",
-                                  "https://tinyurl.com/qz75fbn")
+                                  "www.youtube.com/watch?v=sGbxmsDFVnE")
 fav_movies.append(starWarsForceAwaken)
 
 """using given method to create website"""
